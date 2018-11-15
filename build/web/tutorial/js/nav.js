66eb03778221e1f6dcba06d92f90a83b89596fff
kenny <kennysexton4@gmail.com>
2018-11-15 12:15:25 -0500

    updates and image effect

--- build/web/tutorial/js/nav.js
+++
@@ -1,8 +0,0 @@
-//Script for opening and closing menu
-
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-}
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0px";
-}	

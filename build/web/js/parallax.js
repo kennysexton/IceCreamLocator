e72c24520e19dd0e0e1a28a16914fceb44e3abe4
kenny <kennysexton4@gmail.com>
2018-11-27 16:31:30 -0500

    fixed parallax only loading initially

--- build/web/js/parallax.js
+++ build/web/js/parallax.js
@@ -1,37 +1,39 @@
-$( document ).ready(function() {
-    $('.img-parallax').each(function(){
-      var img = $(this);
-      var imgParent = $(this).parent();
-      function parallaxImg () {
-        var speed = img.data('speed');
-        var imgY = imgParent.offset().top;
-        var winY = $(this).scrollTop();
-        var winH = $(this).height();
-        var parentH = imgParent.innerHeight();
+//      https://codepen.io/RenanB/pen/GZeBNg
+//      Author: Renan Breno
+    
+function parallaxImg () {
 
-     
-        var winBottom = winY + winH;
+var img = $('.img-parallax');
+var imgParent = $('.img-parallax').parent();
 
-        // If block is shown on screen
-        if (winBottom > imgY && winY < imgY + parentH) {
-          // Number of pixels shown after block appear
-          var imgBottom = ((winBottom - imgY) * speed);
-          // Max number of pixels until block disappear
-          var imgTop = winH + parentH;
-          // Percentage between start showing until disappearing
-          var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
-        }
-        img.css({
-          top: imgPercent + '%',
-          transform: 'translate(-50%, -' + imgPercent + '%)'
-        });
-      }
-      $(document).on({
-        scroll: function () {
-          parallaxImg();
-        }, ready: function () {
-          parallaxImg();
-        }
-      });
-    });
+  var speed = img.data('speed');
+  var imgY = imgParent.offset().top;
+  var winY = $(this).scrollTop();
+  var winH = $(this).height();
+  var parentH = imgParent.innerHeight();
+
+
+  var winBottom = winY + winH;
+
+  // If block is shown on screen
+  if (winBottom > imgY && winY < imgY + parentH) {
+    // Number of pixels shown after block appear
+    var imgBottom = ((winBottom - imgY) * speed);
+    // Max number of pixels until block disappear
+    var imgTop = winH + parentH;
+    // Percentage between start showing until disappearing
+    var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+  }
+  img.css({
+    top: imgPercent + '%',
+    transform: 'translate(-50%, -' + imgPercent + '%)'
+  });
+}
+$(document).on({
+  scroll: function () {
+    parallaxImg();
+  }, ready: function () {
+    parallaxImg();
+  }
 });
+    
